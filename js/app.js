e9c5cbc41a26d285cb3ffa53c28879546d7133a7
binod164 <binod7_grg@hotmail.com>
2022-02-11 15:05:25 -0600

    Working on createFruit function

--- js/app.js
+++ js/app.js
@@ -6,16 +6,51 @@
 
 let highScore
 let score
+let boardNumber
+let fruitPosition
 
 /*------------------------ Cached Element References ------------------------*/
+
 const easyBtn = document.querySelector(".easy")
 const mediumBtn = document.querySelector(".medium")
 const hardBtn = document.querySelector(".hard")
 
 /*----------------------------- Event Listeners -----------------------------*/
-easyBtn.addEventListener("click" ,handleCLick)
-mediumBtn.addEventListener("click" ,handleCLickW)
-hardBtn.addEventListener("click" ,handleCLick)
+
+easyBtn.addEventListener("click" ,handleClick)
+mediumBtn.addEventListener("click" ,handleClick)
+hardBtn.addEventListener("click" ,handleClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+function init(){
+
+}
+
+function handleClick(){
+  createSnake();
+  snakeMoves();
+  createFruit();
+}
+
+function createSnake(){
+
+}
+
+function snakeMoves(){
+
+}
+
+function createFruit(){
+// boardNumber = Math.floor(Math.random()*100)
+// console.log(boardNumber)
+// fruitPosition = "sq" + boardNumber.toString()
+// console.log(fruitPosition)
+// const id = document.querySelector(`#${fruitPosition}`)
+// id.innerText = "X"
+// console.log(id)
+
+}
+createFruit()
