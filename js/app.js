16f42ed4936e5b5eadb092ab141c8e6d7b9b0cf8
binod164 <binod7_grg@hotmail.com>
2022-02-11 09:21:43 -0600

    Add level buttons and event listeners to the button

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+let highScore
+let score
+
+/*------------------------ Cached Element References ------------------------*/
+const easyBtn = document.querySelector(".easy")
+const mediumBtn = document.querySelector(".medium")
+const hardBtn = document.querySelector(".hard")
+
+/*----------------------------- Event Listeners -----------------------------*/
+easyBtn.addEventListener("click" ,handleCLick)
+mediumBtn.addEventListener("click" ,handleCLickW)
+hardBtn.addEventListener("click" ,handleCLick)
+
+
+/*-------------------------------- Functions --------------------------------*/
